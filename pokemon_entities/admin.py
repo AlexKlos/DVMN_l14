c4537e845182955987ee54c676badebd5bef9735
AlexKlos <klos@list.ru>
2025-02-03 17:35:50 +0700

    apdated models interface

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from .models import Pokemon, PokemonEntity
 
 
-admin.site.register(Pokemon)
-admin.site.register(PokemonEntity)
+# admin.site.register(Pokemon)
+# admin.site.register(PokemonEntity)
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image')
+
+@admin.register(PokemonEntity)
+class PokemonEntityAdmin(admin.ModelAdmin):
+    list_display = ('latitude', 'longitude')
