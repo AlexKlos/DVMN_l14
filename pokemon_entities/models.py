b7c8ab658e2f872a656ae5be611a18351fbbf77c
AlexKlos <klos@list.ru>
2025-02-03 17:23:54 +0700

    add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,8 +1,14 @@
 from django.db import models  # noqa F401
 
+
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Имя Покемона')
     image = models.ImageField(upload_to='pokemon_images', verbose_name='Изображение Покемона', null=True, blank=True)
 
     def __str__(self):
         return self.title
+    
+
+class PokemonEntity(models.Model):
+    latitude = models.FloatField(verbose_name='lat')
+    longitude = models.FloatField(verbose_name='lon')
